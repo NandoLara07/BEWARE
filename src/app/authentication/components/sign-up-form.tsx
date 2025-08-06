66ff1d4f033db8e702d0a5df25d63adb9f103e44
NandoLara07 <nandolaradossantos@gmail.com>
2025-08-06 18:39:14 -0300

    feat: add sign in with email and password

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -71,11 +71,11 @@ const SignUpForm = () => {
         onError: (error) => {
           if (error.error.code === "USER_ALREADY_EXISTS") {
             toast.error("E-mail já cadastrado.");
-            form.setError("email", {
+            return form.setError("email", {
               message: "E-mail já cadastrado.",
             });
-            toast.error(error.error.message);
           }
+          toast.error(error.error.message);
         },
       },
     });
