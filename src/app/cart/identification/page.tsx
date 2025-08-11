88db1d8b2d981295955ec87f373f2a09533cee70
NandoLara07 <nandolaradossantos@gmail.com>
2025-08-11 16:56:34 -0300

    feat: ensure cart shipping address can be selected

--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -19,9 +19,18 @@ const IdentificationPage = async () => {
   }
 
   const cart = await db.query.cartTable.findFirst({
-    where: eq(cartTable.userId, session.user.id),
+    where: (cart, { eq }) => eq(cart.userId, session.user.id),
     with: {
-      items: true,
+      shippingAddress: true,
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
     },
   });
   if (!cart || cart?.items.length === 0) {
@@ -30,12 +39,14 @@ const IdentificationPage = async () => {
   const shippingAddresses = await db.query.shippingAddressTable.findMany({
     where: eq(shippingAddressTable.userId, session.user.id),
   });
-
   return (
     <>
       <Header />
       <div className="px-5">
-        <Addresses shippingAddresses={shippingAddresses} />
+        <Addresses
+          shippingAddresses={shippingAddresses}
+          defaultShippingAddressId={cart.shippingAddress?.id || null}
+        />
       </div>
     </>
   );
