3dc2bb5a252840d2b99f1a0c26fc7c067e211752
NandoLara07 <nandolaradossantos@gmail.com>
2025-08-13 20:53:49 -0300

    refactor: add data access layer functions to get poducts and categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
@@ -6,24 +5,18 @@ import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import Partners from "@/components/common/partners";
 import ProductList from "@/components/common/product-list";
-import { db } from "@/db";
-import { productTable } from "@/db/schema";
+import { getCategories } from "@/data/categories/get";
+import {
+  getNewlyCreatedProducts,
+  getProductsWithVariants,
+} from "@/data/products/get";
 
 const Home = async () => {
-  const products = await db.query.productTable.findMany({
-    with: {
-      variants: true,
-    },
-  });
-
-  const newlyCreatedProducts = await db.query.productTable.findMany({
-    orderBy: [desc(productTable.createdAt)],
-    with: {
-      variants: true,
-    },
-  });
-
-  const categories = await db.query.categoryTable.findMany({});
+  const [products, newlyCreatedProducts, categories] = await Promise.all([
+    getProductsWithVariants(),
+    getNewlyCreatedProducts(),
+    getCategories(),
+  ]);
   return (
     <>
       <Header />
